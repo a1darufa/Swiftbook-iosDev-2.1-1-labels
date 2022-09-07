596907a6155e63ee70a0cd0e9635c7b3d346df92
Aidar <a1dar.ufa@gmail.com>
2022-09-07 11:07:03 +0500

    Initial Commit

---
+++ Swiftbook-iosDev-2.1-1-labels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swiftbook-iosDev-2.1-1-labels
+//
+//  Created by Айдар Абдуллин on 07.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
